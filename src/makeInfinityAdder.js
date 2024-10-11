aff7fc8fe5b21ae99067f47c38755d46abda84b2
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-10-11 17:55:43 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sumOfAllNumbers = 0;
+  let copyOfSum;
+
+  const adder = (number = null) => {
+    sumOfAllNumbers += number;
+
+    if (number === null) {
+      copyOfSum = sumOfAllNumbers;
+      sumOfAllNumbers = 0;
+
+      return copyOfSum;
+    }
+
+    return adder;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
